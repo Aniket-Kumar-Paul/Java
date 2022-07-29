2e0890476ca94854ba8de1a0e42520825b065fa2
Aniket <aniketkumarpaul1@gmail.com>
2022-07-29 21:11:13 +0530

    2D Arrays

--- Main.java
+++ Main.java
@@ -2,23 +2,20 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // Arrays
         Scanner sc = new Scanner(System.in);
-        int size = sc.nextInt();
-        int numbers[] = new int[size];
+        int rows = sc.nextInt();
+        int cols = sc.nextInt();
 
-        // Input
-        for (int i = 0; i < size; i++) {
-            numbers[i] = sc.nextInt();
+        // 2D Arrays
+        int[][] numbers = new int[3][5];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++)
+                numbers[i][j] = sc.nextInt();
         }
-
-        int x = sc.nextInt(); // search for x
-
-        // Search
-        for (int i = 0; i < numbers.length; i++) {
-            if (numbers[i] == x) {
-                System.out.println(x + " found at: " + i);
-            }
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++)
+                System.out.print(numbers[i][j]+" ");
+            System.out.println();
         }
     }
 }
