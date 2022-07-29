7232ea14bd96bc77ba6464d37e10ab69d39607e4
Aniket <aniketkumarpaul1@gmail.com>
2022-07-29 22:36:56 +0530

    string builder

--- Main.java
+++ Main.java
@@ -1,47 +1,31 @@
 import java.util.Scanner;
 
+import javax.swing.border.SoftBevelBorder;
+
 public class Main {
     public static void main(String[] args) {
-        // Strings --> Immutable
-        String myName = "Aniket Paul";
-        Scanner sc = new Scanner(System.in);
-        String yourName = sc.nextLine();
-        System.out.println("Your name is : " + yourName);
+        // String Builder -> used for efficient string operations as strings are immutable
+        StringBuilder sb = new StringBuilder("Aniket");
+        System.out.println(sb);
+        System.out.println(sb.charAt(1));
+        
+        // setCharAt - changing the string (builder)
+        sb.setCharAt(0, 'C'); // (index, 'single character')
+        System.out.println(sb);
 
-        // String functions 
-        String s1 = "Hey";
-        String s2 = "Hi";
-        String s3 = s1 + " " + s2; // concatenation
-        System.out.println(s3);
-        System.out.println(s3.length()); // length
+        // insert
+        sb.insert(1, "hi"); // (index, "string")
+        System.out.println(sb);
 
-        for (int i=0; i<s3.length(); i++) {
-            System.out.println(s3.charAt(i)); // charAt
-        }
+        // delete
+        sb.delete(2, 4); // (start, end) [excluding end]
+        System.out.println(sb); 
 
-        // compareTo 
-        //      - s1 > s2 : +ve value
-        //      - s1 = s2 : 0
-        //      - s1 < s2 : -ve value
-        if(s1.compareTo(s2) == 0) {
-            System.out.println("Strings are same");
-        } else {
-            System.out.println("Strings are not same");
-        }
+        // append
+        sb.append("er");
+        System.out.println(sb);
 
-        // substring(beg index, end index) [end excluded]
-        String firstName = myName.substring(0, 6);
-        // String lastName = myName.substring(7, myName.length());
-        String lastName = myName.substring(7); // same as above (takes last index as end index by default)
-        System.out.println(firstName);
-        System.out.println(lastName);
-
-        // parseInt 
-        String str = "123";
-        int number = Integer.parseInt(str);
-        System.out.println(number);
-
-        // toString
-        String newstr = Integer.toString(number);
+        // length
+        System.out.println(sb.length());
     }
 }
